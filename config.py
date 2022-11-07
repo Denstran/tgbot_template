2bf2233473843f762caef68d5143eaadc81c8efc
Kirill Stradze <akamanapa@gmail.com>
2022-11-07 23:54:50 +0300

    Modificate template

--- config.py
+++ config.py
@@ -1,8 +1,19 @@
 from dataclasses import dataclass
+from environs import Env
+
+env = Env()
+env.read_env()
 
 
 @dataclass
 class Config:
-    token: str = 'YOUR_TOKEN'
-    payment_token: str = 'PAYMENT_TOKEN'
-    admin_ids: int = 694767747
+    token = env.str('BOT_TOKEN')
+    payment_token = env.str('PAYMENT_TOKEN')
+    admin_ids = list[map[int, env.list('ADMINS')]]
+
+
+@dataclass
+class DBConfig:
+    db_password = env.str('DB_PASSWORD')
+    db_user = env.str('DB_USER')
+    db_name = env.str('DB_NAME')
